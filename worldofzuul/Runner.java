dc04ef8f4001bef95fc63ec5adc5057efb5cce1b
Casper <capper2704@hotmail.dk>
2021-10-14 11:06:28 +0200

    Casper Tester

--- worldofzuul/Runner.java
+++ worldofzuul/Runner.java
@@ -4,9 +4,8 @@ public class Runner {
     public static void main(String[] args){
         Game game = new Game();
         System.out.println("test");
-
         System.out.println("live test");
-        System.out.println();
+        System.out.println("yeet");
         System.out.println("Thor er sej");
         game.play();
 
