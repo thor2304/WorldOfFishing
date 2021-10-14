0eeecd114bed38483dbb6d2404b1e93589ff8dad
Thor Jørgin <thor.malmby@gmail.com>
2021-10-14 10:36:04 +0200

    test push
    puha

--- worldofzuul/Runner.java
+++ worldofzuul/Runner.java
@@ -3,7 +3,7 @@ package worldofzuul;
 public class Runner {
     public static void main(String[] args){
         Game game = new Game();
-
+        System.out.println("test");
         game.play();
 
     }
