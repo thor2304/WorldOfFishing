02e0183f3bfca26335f4ae1a7885ede1b7bf98bf
Thor Jørgin <thor.malmby@gmail.com>
2021-10-14 10:41:09 +0200

    test 2

--- worldofzuul/Runner.java
+++ worldofzuul/Runner.java
@@ -4,6 +4,8 @@ public class Runner {
     public static void main(String[] args){
         Game game = new Game();
         System.out.println("test");
+
+        System.out.println("live test");
         game.play();
 
     }
