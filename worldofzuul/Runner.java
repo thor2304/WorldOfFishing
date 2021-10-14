4055cd60647dd7e48e57f7d58c823d63287a7878
Suhaib Aslam <suhaib_aslam@hotmail.com>
2021-10-14 11:33:16 +0200

    Test2 Suhaib

--- worldofzuul/Runner.java
+++ worldofzuul/Runner.java
@@ -10,6 +10,7 @@ public class Runner {
         System.out.println("Thor er sej");
         System.out.println("Daniella test");
         System.out.println("Suhaib");
+        System.out.println("Suhaib2_test");
         game.play();
 
     }
