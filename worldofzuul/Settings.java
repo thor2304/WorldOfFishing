f44898046fdb8bd528488e1d850dcc07b5e3c661
Aisha <aimoh21@student.sdu.dk>
2021-11-02 13:40:16 +0100

    created a Settings class

---
+++ worldofzuul/Settings.java
@@ -0,0 +1,13 @@
+package worldofzuul;
+
+/**
+ * this class defines default settings to make it more manageable.
+ */
+public class Settings {
+    public static final double DEFAULTHABITATQUALITY = 1;
+    public static final int DEFAULTNUMBEROFFISH = 1000;
+    public static final double DEFAULTNETDESTRUCTION = 0.1;
+    public static final double DEFAULTCATCHRATE = 0.06;
+    public static final int BOARDSIZE = 3;
+    public static final double VARIANCE = 0.2;
+}
