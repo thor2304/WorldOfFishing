a774a7051e7fe67ed1fb8a493ad0aca17de08776
Aisha <aimoh21@student.sdu.dk>
2021-11-05 13:55:09 +0100

    more comments added

--- worldofzuul/Settings.java
+++ worldofzuul/Settings.java
@@ -4,27 +4,38 @@ package worldofzuul;
  * this class defines default settings to make it more manageable.
  */
 public class Settings {
-    /**
+    /** the default number of the habitat quality in the tile.
      *
      */
     public static final double DEFAULTHABITATQUALITY = 1;
-    /**
+
+
+    /**the default number of fish.
      *
      */
     public static final int DEFAULTNUMBEROFFISH = 1000;
-    /**
+
+
+    /** the default number of the net destruction.
      *
      */
     public static final double DEFAULTNETDESTRUCTION = 0.1;
-    /**
+
+
+
+    /** the default catch rate.
      *
      */
     public static final double DEFAULTCATCHRATE = 0.06;
-    /**
+
+
+    /** the board size of the tile.
      *
      */
     public static final int BOARDSIZE = 3;
-    /**
+
+
+    /** the amount of variance of the caught fish.
      *
      */
     public static final double VARIANCE = 0.2;
