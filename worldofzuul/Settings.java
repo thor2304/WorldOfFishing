1d5b3f7ce7e5982cade44b033c2d4dce2ac5d1cc
Aisha <aimoh21@student.sdu.dk>
2021-11-05 14:47:56 +0100

    added docstrings and links in settings class

--- worldofzuul/Settings.java
+++ worldofzuul/Settings.java
@@ -4,39 +4,39 @@ package worldofzuul;
  * this class defines default settings to make it more manageable.
  */
 public class Settings {
-    /** the default number of the habitat quality in the tile.
-     *
+    /** the default number of the habitat quality in the tile.{@link Tile#Tile(String)}
+     * click link to read what the method does.
      */
     public static final double DEFAULTHABITATQUALITY = 1;
 
 
-    /**the default number of fish.
-     *
+    /** the default number of fish.{@link Tile#Tile(String)}
+     * click link to read what the method does.
      */
     public static final int DEFAULTNUMBEROFFISH = 1000;
 
 
-    /** the default number of the net destruction.
-     *
+    /** the default number of the net destruction.{@link Tile#fishTile(int)}
+     * click link to read what the method does.
      */
     public static final double DEFAULTNETDESTRUCTION = 0.1;
 
 
 
-    /** the default catch rate.
-     *
+    /** the default catch rate.{@link Tile#fishTile(int)}
+     * click link to read what the method does.
      */
     public static final double DEFAULTCATCHRATE = 0.06;
 
 
-    /** the board size of the tile.
+    /** the board size of the tile.{@link Game()}
      *
      */
     public static final int BOARDSIZE = 3;
 
 
-    /** the amount of variance of the caught fish.
-     *
+    /** the amount of variance of the caught fish.{@link Tile#fishTile(int, double, double)}
+     * click link to read what the method does.
      */
     public static final double VARIANCE = 0.2;
 }
